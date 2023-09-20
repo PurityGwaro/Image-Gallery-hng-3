a22e8f395f1207b4db97922666c5f29146d51abd
PurityGwaro <puritygwaro@gmail.com>
2023-09-20 23:24:38 +0300

    moving the images on phone

--- README.md
+++ README.md
@@ -14,21 +14,8 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+## Interacting with the site
+ Enter username: `user@example.com`
+ Enter password: `1Password`
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+ You can then search for anything you'd like and will be able to move images around as you please.
--- app/components/DraggableImageContainer.js
+++ app/components/DraggableImageContainer.js
@@ -2,23 +2,45 @@ import React from "react";
 import ImageItem from "./ImageItem";
 
 function DraggableImageContainer({ images, onImageMove }) {
+  const handleDragStart = (e, index) => {
+    e?.dataTransfer?.setData("imageIndex", index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, targetIndex) => {
+    e.preventDefault();
+    const sourceIndex = e?.dataTransfer?.getData("imageIndex");
+    onImageMove(sourceIndex, targetIndex);
+  };
 
   return (
     <div className="container pt-10 pb-20 mx-auto">
-      <ul className="grid items-center justify-center w-full h-full gap-20 px-6 md:px-0 lg:grid-cols-4 md:grid-cols-2 place-content-center">
+      <ul
+        className="grid items-center justify-center w-full h-full gap-20 px-6 md:px-0 lg:grid-cols-4 md:grid-cols-2 place-content-center"
+        onTouchStart={(e) => {
+          const touch = e.touches[0];
+          const li = touch.target.closest("li");
+          if (li) {
+            const index = Array.from(li.parentNode.children).indexOf(li);
+            handleDragStart(e, index);
+          }
+        }}
+      >
         {images.map((image, index) => (
           <li
-          key={image.id} 
-          draggable
-          onDragStart={(e) => {
-            e.dataTransfer.setData('imageIndex', index);
-          }}
-          onDragOver={(e) => e.preventDefault()}
-          onDrop={(e) => {
-            e.preventDefault();
-            const sourceIndex = e.dataTransfer.getData('imageIndex');
-            onImageMove(sourceIndex, index);
-          }}
+            key={image.id}
+            draggable
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, index)}
+            onTouchStart={(e) => handleDragStart(e, index)}
+            onTouchMove={(e) => {
+              e.preventDefault();
+            }}
+            onTouchEnd={(e) => handleDrop(e, index)}
           >
             <ImageItem image={image} />
           </li>
