e5d253201faba23f365e76e74a8f33bb09217c06
PurityGwaro <puritygwaro@gmail.com>
2023-09-20 15:25:03 +0300

    fix draggable

--- app/components/DraggableImageContainer.js
+++ app/components/DraggableImageContainer.js
@@ -3,18 +3,19 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import ImageItem from "./ImageItem";
 
 function DraggableImageContainer({ images, onDragEnd }) {
+  console.log('images: ', images)
   return (
     <div className="container pt-10 pb-20 mx-auto">
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="imageGrid" direction="horizontal">
+        <Droppable droppableId="imageGrid">
           {(provided) => (
             <div
               {...provided.droppableProps}
               ref={provided.innerRef}
-              className="grid items-center justify-center w-full gap-20 px-6 md:px-0 lg:grid-cols-4 md:grid-cols-2 place-content-center"
+              className="grid items-center justify-center w-full h-full gap-20 px-6 md:px-0 lg:grid-cols-4 md:grid-cols-2 place-content-center"
             >
               {images.map((image, index) => (
-                <Draggable key={image.id} draggableId={image.id} index={index}>
+                <Draggable key={image.id} draggableId={image.id.toString()} index={index}>
                   {(provided) => (
                     <div
                       ref={provided.innerRef}
--- app/components/ImageItem.js
+++ app/components/ImageItem.js
@@ -4,13 +4,16 @@ import React from "react";
 function ImageItem({ image }) {
   // console.log('image being received: ', image)
   return (
-    <Image
+    <div>
+      <Image
       src={image.src.large2x}
       height={250}
       width={250}
       alt={image.alt}
       className="h-auto border-2 shadow-xl md:w-[300px] rounded-2xl"
     />
+    <span>#{image.photographer}</span>
+    </div>
   );
 }
 
